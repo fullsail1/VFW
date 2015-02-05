77ab8afd02759b1ba9380edbbf1ef989a6f047c8
Hicks_Anthony_SDI <osahon10@fullsail.edu>
2015-02-05 15:09:10 -0500

    completed group assignment
    did that

---
+++ VFW_Lecture/Day 2/Resources/app.js
@@ -0,0 +1,18 @@
+var wit = Titanium.UI.createWindow({
+	backgroundColor: "#2E8A6A",
+	
+	
+	
+});
+var view = Titanium.UI.createView({
+	//backgroundColor: "#fff",
+	backgroundImage: "jacket.jpg",
+	width: 500,
+	height:500,
+	
+});
+
+wit.add(view);
+wit.open();
+
+
